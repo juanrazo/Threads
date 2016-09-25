1a4cebed05e119e3a0447a4e200ce04493f1ba5d
David Pruitt <daviddpruitt@gmail.com>
2016-09-24 20:05:50 -0600

    Added comments to simple bank demo

--- demo-bank/bank_simple.c
+++ demo-bank/bank_simple.c
@@ -1,3 +1,11 @@
+/** \file bank_simple.c
+ *  \brief This is a simple bank transaction demo using threads.
+ *  This demo creates 3 atms and 2 accounts, only one account is
+ *  used. Each ATM runs within a seperate thread and all ATMs do
+ *  transactions on the same account. There is no coordination
+ *  between the threads and the transactions occur concurrently.
+ *
+ */
 #include "stdlib.h"
 #include "tthreads.h"
 #include "stdio.h"
@@ -7,26 +15,37 @@
 #  define yield()
 #endif
 
+/** ATM struct, contains and ID and tracks cashflow */
 typedef struct {
-  int atmId;			/* id of this ATM */
-  int cashFlowOut;		/* net amount withdrawn */
+  int atmId;			        /**< id of this ATM */
+  int cashFlowOut;		        /**< net amount withdrawn */
 } ATM;
 
+/** Account struct, contains account number and balance */
 typedef struct {
-  int accountId; 
+  int accountId;                        
   int balance;
-  Mutex lock;
 } Account;
 
-ATM atms[3] = { {0,0}, {1,0}, {2,0} }; /* 3 atms, zero dollars */
-Account accounts[2] = {{0,0}, {1,0}}; /* 2 accounts, zero balances */
-  
+ATM atms[3] = { {0,0}, {1,0}, {2,0} };  /**<  3 atms, zero dollars */
+Account accounts[2] = {{0,0}, {1,0}};   /**<  2 accounts, zero balances */
+
+/** Withdraw money from an account. If the requested amount
+ *  is available withdraws the amount from specified account
+ *
+ *  \param atm The atm attempting the withdrawal
+ *  \param account The account being withdrawn from
+ *  \param request The amount to withdraw
+ *  \return Returns 1 if withdrawal was succesful, 0 otherwise
+ *  \sa doWithdraws() doDeposit()
+ */
 int
 doWithdraw(ATM *atm, Account *account, int request)
 {
-  int bal = account->balance, success = 0;
+  int bal = account->balance,           /**< balance of the account */
+      success = 0;                      /**< success status */
   yield();
-  if (bal >= request) {
+  if (bal >= request) {                 /* check if enough in account */
     bal -= request;
     yield();
     account->balance = bal;
@@ -36,6 +55,11 @@ doWithdraw(ATM *atm, Account *account, int request)
   return success;
 }
 
+/** Makes 3 withdrawals from account 0 in the amount of 7
+ *  
+ *  \param atm The atm where the withdrawals are being made
+ *  \sa doWithdraw
+ */
 void 
 doWithdraws(ATM *atm)
 {
@@ -46,6 +70,14 @@ doWithdraws(ATM *atm)
   }
 }
 
+/** Deposits money into an account. If the requested amount
+ *  is available withdraws the amount from specified account
+ *
+ *  \param atm The atm where the deposit is being made
+ *  \param account The account being deposited to
+ *  \param request The amount to deposit
+ *  \sa doDeposits() doWithdraws()
+ */
 void
 doDeposit(ATM *atm, Account *account, int request)
 {
@@ -56,6 +88,10 @@ doDeposit(ATM *atm, Account *account, int request)
   atm->cashFlowOut -= request;
 }
 
+/** Makes 3 deposits into account 0 in the amount of 9.
+ *
+ *  \param atm The atm where the deposits are being made
+ */
 void 
 doDeposits(ATM *atm)
 {
@@ -66,14 +102,13 @@ doDeposits(ATM *atm)
   }
 }
 
-
-
-
-
-
+/** Initilazes everything, starts, threads, and prints results
+ */
 int main()
 {
-  int numThreads = 3, i, sumFlowOut = 0;
+  int numThreads = 3,                   /**< Number of threads */
+      i,
+      sumFlowOut = 0;                   /**< Total cashflow from all ATMs */
   makeThreads(numThreads);
 
   /* initialize account mutexes (unlocked) */
