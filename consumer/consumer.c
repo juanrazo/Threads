e106808da23aaafaf8c9c73c24acf400d0585263
juanrazo <jrazo@gmail.com>
2016-10-21 15:15:57 -0600

    comments

--- consumer/consumer.c
+++ consumer/consumer.c
@@ -5,12 +5,17 @@
 #include "mutex.h"
 
 #define MAX 10
+#define THREADS 5
+
 Semaphore empty;
 Semaphore full;
 
+//Flag to keep track of Producers
 int flag = 0;
+
+//Buffer with size 10
 typedef struct{
-  int queue[10];
+  int queue[MAX];
   int front;
   int rear;
   int count;
@@ -18,11 +23,13 @@ typedef struct{
 }Buffer;
 Buffer buff;
 
+// Initialize the semaphores
 void initSemaphores(){
   semaphoreInit(&empty, MAX);
   semaphoreInit(&full, 0);
 }
 
+// Initialize the buffer
 void initBuff(){
   Buffer *b = &buff;
   
@@ -32,6 +39,11 @@ void initBuff(){
   mutexInit(&b->lock, 1);
 }
 
+/* Functions for Buffer queue
+   The idea and some code for the queue
+   comes from tutorialspoint.com
+   tutorialspoint.com/data_structures_algorithms/queue_program_in_c.htm
+*/
 int isFull(Buffer *buffer){
   return (buffer->count == MAX) ? 1 : 0;
 }
@@ -71,45 +83,69 @@ int isBufferEmpty(){
   mutexUnlock(&(&buff)->lock);
   return num;
 }
+
+/* 
+   The producer code comes from the OS book, section
+   2.3.5 Semaphores. The only Difference is the 
+   print, incrase item and the flag when its done
+   producing. 
+*/
 void producer(){
   int item = 0;
   
   while(item <100){
+
     down(&empty);
     mutexLock(&(&buff)->lock);
+
     enqueue(&buff, item);
+
     mutexUnlock(&(&buff)->lock);
     up(&full);
-    printf("Pushed %d\n", item);
+
+    printf("Produced %d\n", item);
     item++;
   }
   flag++;
 }
-
+/* 
+   The consumer is also from the OS book on section
+   2.3.5. The total variable is to keep track of the
+   produced integers. The consume is just to save the
+   dequeue value and the consumeFlag is to check if
+   all producers are done. The consumer will stop
+   consuming when the all producers are done and the
+   buffer is empty.
+*/
 void consumer(){
   int total = 0;
   int consume;
   int consumeFlag=1;
   while(consumeFlag){
+
     down(&full);
     mutexLock(&(&buff)->lock);
+
     consume = dequeue(&buff);
-    printf("Poped %d\n", consume);
+    printf("Consumed %d\n", consume);
+
     mutexUnlock(&(&buff)->lock);
     up(&empty);
     total = total + consume;
     printf("Total count %d\n", total);
-    if(flag ==4 && isBufferEmpty())
+
+    if((flag == (THREADS-1)) && isBufferEmpty())
       consumeFlag=0;
   }
+  printf("Done consuming %d\n", total);
 }
 
 int main(){
-  int numThreads = 5;
+ 
   initSemaphores();
   initBuff();
 
-  makeThreads(numThreads);
+  makeThreads(THREADS);
 
   createThread((void (*)(void *))producer, &buff);
   createThread((void (*)(void *))producer, &buff);
@@ -119,5 +155,6 @@ int main(){
 
   startThreads();
   printf("Threads Finished \n");
+
   return 1;
 }
